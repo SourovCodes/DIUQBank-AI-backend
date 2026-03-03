8bb2b628a15a1b402a11d8bcd02b53246a5cfe09
Sourov Biswas <sourovcodes@gmail.com>
2026-03-04 00:03:56 +0600

    semester

---
+++ app/Filament/Resources/Semesters/Tables/SemestersTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\Semesters\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SemestersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->weight('medium'),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->emptyStateHeading('No semesters found')
+            ->emptyStateDescription('Create your first semester to organize questions.')
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
