8e371d90f634e9a6cded1f94c649faf760c3a70d
Sourov Biswas <sourovcodes@gmail.com>
2026-03-16 21:34:11 +0600

    Remove obsolete tests for admin registration, guarded delete action, and quick upload resource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Filament\Forms\Components\TextInput;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        TextInput::configureUsing(function (TextInput $component): void {
-            $component->trim();
-        });
+        //
     }
 }
